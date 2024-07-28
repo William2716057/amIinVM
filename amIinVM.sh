dc012cdec0f0aa17b66320d24bf829b9ce7ed9f9
User <Willspri@gmail.com>
2024-07-28 20:26:21 +1200

    added colour

--- amIinVM.sh
+++ amIinVM.sh
@@ -2,23 +2,27 @@
 
 # List of files and directories to search for
 targets=(
-    "/etc/vmware-tools/"
-    "/etc/qemu/"
-    "/etc/virtualbox/"
-    "/var/lib/libvirt/"
-    "/var/log/vmware/"
-    "/usr/bin/vmware-user-suid-wrapper"
-    "/usr/sbin/VBoxService"
-    "/var/run/vboxadd-service.sh"
-    "/etc/init.d/vboxadd-service"
-    "/usr/bin/vboxcontrol"
+	"/etc/vmware-tools/"
+	"/etc/qemu/"
+	"/etc/virtualbox/"
+	"/var/lib/libvirt/"
+	"/var/log/vmware/"
+	"/usr/bin/vmware-user-suid-wrapper"
+	"/usr/sbin/VBoxService"
+	"/var/run/vboxadd-service.sh"
+	"/etc/init.d/vboxadd-service"
+	"/usr/bin/vboxcontrol"
 )
+
+RED='\033[0;31m'
+NC='\033[0m'
 
 # Loop through each target and check if it exists
 for target in "${targets[@]}"; do
-    if [ -e "$target" ]; then
-        echo "Found: $target"
-    else
-        echo "Not found: $target"
-    fi
+	if [ -e "$target" ]; then
+    	echo -e "${RED}Found: $target${NC}"
+	else
+    	echo "Not found: $target"
+	fi
 done
+
